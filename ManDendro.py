b0fde7dc9082a8fee628b70d8070c1f393f58e47
jksr <jksr.tw@gmail.com>
2022-06-22 22:40:48 -0700

    add auxiliary color in plot

--- ManDendro.py
+++ ManDendro.py
@@ -2,11 +2,19 @@ import pandas as pd
 import numpy as np
 
 class ManDendro:
-    def __init__(self,Z, labels):
+    def __init__(self,Z, labels, labelcolors=None):
         self.Zdf = self.prepare_link_df(Z)
         self.currZdf = self.Zdf.copy()
         self.opq = []
         self.labels = list(labels)
+
+        if labelcolors is not None:
+            colormap = dict()
+        if isinstance(labelcolors, dict):
+            colormap = labelcolors
+        else:
+            colormap = dict(zip(self.labels, labelcolors))
+        self.colormap = {x:colormap.get(x,'#ffffff') for x in labels}
     
     @staticmethod
     def prepare_link_df(Z):
@@ -25,13 +33,13 @@ class ManDendro:
     def undo(self):
         self.switch_node(self.opq.pop())
     
-    def plot_dendro(self, Zdf=None, ax=None):
+    def plot_dendro(self, Zdf=None, ax=None, labelcolors=True):
         import scipy.cluster.hierarchy as sch
         if Zdf is None:
             Zdf = self.currZdf
         if ax is None:
             import matplotlib.pyplot as plt
-            fig, ax = plt.subplots(1,1,figsize=(10,3))
+            fig, ax = plt.subplots(1,1,figsize=(20,6))
         
         ilabels = [f'{x.Index}={x.left}+{x.right}' for x in self.Zdf.itertuples()]
         
@@ -42,6 +50,14 @@ class ManDendro:
             y = dcoord[1]
             ind = np.nonzero(ii == j)[0][0]
             ax.annotate(ilabels[ind], (x,y), va='top', ha='center')
+        
+        positions = ax.get_xticks()
+        texts = ax.get_xticklabels()
+        w = (positions[1]-positions[0])/2
+        for pos,txt in zip(positions, texts):
+            txt = txt.get_text()
+            ax.axvspan(pos-w, pos+w, facecolor=self.colormap[txt], alpha=0.5)
+        # plt.gca().axis('off')    
             
         return ax
     
